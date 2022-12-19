f7b4189a32edf5c185f47b7290e18684ba0a05d2
Edmétrio J <edmetrio06@gmail.com>
2022-12-19 16:06:45 +0200

    add cargo

--- app/Http/Livewire/Agente.php
+++ app/Http/Livewire/Agente.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Models\Agente as ModelsAgente;
+use App\Models\Models\Cargo;
 use App\Models\Models\Categoria;
 use App\Models\Models\Tipo;
 use Livewire\Component;
@@ -14,7 +15,9 @@ class Agente extends Component
     public function render()
     {
         $this->tipo = Tipo::orderBy('created_at', 'desc')->get();
-        $this->agente = ModelsAgente::with('propriedades','distritos')->where('nome' , 'like', '%' . $this->search . '%')->orderBy('created_at','desc')->get();
+        $this->agente = ModelsAgente::with('propriedades','distritos','cargos')->where('nome' , 'like', '%' . $this->search . '%')->orderBy('created_at','desc')->get();
+        $this->agentes = Cargo::with('agentes')->orderBy('created_at', 'desc')->get();
+        /* dd($this->agentes); */
         $tipo = Tipo::orderBy('created_at', 'desc')->get();
         $categoria = Categoria::orderBy('created_at', 'desc')->get();
         return view('livewire.agente')->layout('layouts.app', compact('categoria','tipo'));
